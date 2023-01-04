d7790bdc795bb5d1063fa860e4b2012c03f030d3
atokarew <free_m1nd@mail.ru>
2023-01-04 13:41:52 +0300

    137

--- dlgo/nn/layers.py
+++ dlgo/nn/layers.py
@@ -8,6 +8,9 @@ def sigmoid(z):
     return np.vectorize(sigmoid_double)(z)
 
 def sigmoid_prime_double(x):
+    return sigmoid_double(x) * (1 - sigmoid_double(x))
+
+def sigmoid_prime(z):
     return np.vectorize(sigmoid_prime_double)(z)
 
 class Layer:
@@ -38,7 +41,7 @@ class Layer:
             return self.input_data
         
     def backward(self):
-        raise
+        raise NotImplementedError
     
     def get_backward_input(self):
         if self.next is not None:
@@ -69,4 +72,53 @@ class ActivationLayer(Layer):
     def backward(self):
         delta = self.get_backward_input()
         data = self.get_forward_input()
-        self.output_data = sigmoid(data)
+        self.output_data = delta * sigmoid_prime(data)
+        
+    def describe(self):
+        print("|--" + self.__class__.__name__)
+        print("  |-- dimensions: ({},{})"
+            .format(self.input_dim, self.output_dim))
+        
+class DenseLayer(Layer):
+    
+    def __init__(self, input_dim, output_dim):
+        
+        super(DenseLayer, self).__init__()
+        
+        self.input_dim = input_dim
+        self.output_dim = output_dim
+        
+        self.weigth = np.random.randn(output_dim, input_dim)
+        self.bias = np.random.randn(output_dim, 1)
+        
+        self.params = [self.weigth, self.bias]
+        
+        self.delta_w = np.zeros(self.weigth.shape)
+        self.delta_b = np.zeros(self.bias.shape)
+        
+    def forward(self):
+        data = self.get_forward_input()
+        self.output_data = np.dot(self.weigth, data) + self.bias
+        
+    def backward(self):
+        data = self.get_forward_input()
+        delta = self.get_backward_input()
+        
+        self.delta_b += delta
+        self.delta_w += np.dot(delta, data.transpose())
+        self.output_delta = np.dot(self.weigth.transpose(), delta)
+        
+    def update_params(self, rate):
+        self.weigth -= rate * self.delta_w
+        self.bias -= rate * self.delta_b
+        
+    def clear_deltas(self):
+        self.delta_w = np.zeros(self.weigth.shape)
+        self.delta_b = np.zeros(self.bias.shape)
+        
+    def describe(self):
+        print("|--- " + self.__class__.__name__)
+        print("  |-- dimensions: ({},{})"
+              .format(self.input_dim, self.output_dim))
+    
+        
