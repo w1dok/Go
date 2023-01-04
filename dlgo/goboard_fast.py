970d0e37bdcaaf0a390368e56c18d28d2810a89b
atokarew <free_m1nd@mail.ru>
2023-01-04 18:21:24 +0300

    scoring

---
+++ dlgo/goboard_fast.py
@@ -0,0 +1,3 @@
+import copy
+from dlgo.gotypes import Player, Point
+from dlgo.scoring import computer_game_result
---
+++ generate_mcts_games.py
@@ -0,0 +1,5 @@
+import argparse
+import numpy as np
+
+from dlgo.encoders import get_encoder_by_name
+from dlgo import goboard_fast as goboard
