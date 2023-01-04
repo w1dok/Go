b116c3ae49301cb810f43402d6661b1e81b0e41c
atokarew <free_m1nd@mail.ru>
2023-01-04 18:54:23 +0300

    goboard_fast_1

--- dlgo/goboard_fast.py
+++ dlgo/goboard_fast.py
@@ -1,3 +1,65 @@
 import copy
 from dlgo.gotypes import Player, Point
 from dlgo.scoring import computer_game_result
+from dlgo import zobrist
+from dlgo.utils import MoveAge
+
+__all__ = [
+    'Board',
+    'GameState',
+    'Move',
+]
+
+neighbot_tables = {}
+corner_tables = {}
+
+def init_neighbor_table(dim):
+    rows, cols = dim
+    new_table = {}
+    for r in range(1 , row + 1):
+        for c in range(1, cols + 1):
+            p = Point(row=r, col=c)
+            full_neighbors = p.neighbors()
+            true_neighbors = [
+                n for n in full_neighbors
+                if 1 <= n.row <= rows and 1 <= n.col <= cols]
+            new_table[p] = true_neighbors
+    neighbot_tables[dim] = new_table
+
+def init_corner_table(dim):
+    rows, cols = dim
+    new_table = {}
+    for r in range(1, rows + 1):
+        for c in range(1, cols + 1):
+            p = Point(row=r, col=c)
+            full_corners = [
+                Point(row=p.row - 1, col=p.col - 1),
+                
+            ]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- dlgo/utils.py
+++ dlgo/utils.py
@@ -1,3 +1,8 @@
+import platform
+import subprocess
+
+import numpy as np
+
 from dlgo import gotypes
 
 COLS = 'ABCDEFGHJKLMNOPQRST'
@@ -30,3 +35,28 @@ def point_from_coords(coords):
     col = COLS.index(coords[0]) + 1
     row = int(coords[1:])
     return gotypes.Point(row=row, col=col)
+
+def coords_from_point(point):
+    return '%s%d' % (
+        COLS[point.col - 1],
+        point.row
+    )
+
+def clear_screen():
+    if platform.system() == "Windows":
+        subprocess.Popen("cls", shell=True).communicate()
+    else:
+        print(chr(27) + "[2J")
+
+class MoveAge():
+    def __init__(self, board):
+        self.move_ages = - np.ones((board.num_rows, board.num_cols))
+
+    def get(self, row, col):
+        return self.move_ages[row, col]
+
+    def reset_age(self, point):
+        self.move_ages[point.row - 1, point.col - 1] = 0
+
+    def increment_all(self):
+        self.move_ages[self.move_ages > -1] += 1
